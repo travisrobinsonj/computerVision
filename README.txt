dde6e916f086388f6013c4ef2b435e2a5a7d81ad
Tajudeen Abdulazeez <teejay4show@yahoo.com>
2018-07-28 00:23:23 -0700

    update readme and liveSketch.py

--- README.txt
+++ README.txt
@@ -1,1 +1,8 @@
 Computer Vision Project with OpenCV
+
+Live sketch usage and sketch is save in current directory when enter key is press:
+   python liveSketch.py
+
+Plotting color distribution of an image in histogram
+   python imageHist.py --image <image directory>
+
--- liveSketch.py
+++ liveSketch.py
@@ -21,19 +21,20 @@ def sketch(image):
 	#do an invert binarize the image
 	ret, mask = cv2.threshold(canny_edges, 70 , 255, cv2.THRESH_BINARY_INV)
 	return mask
+if __name__ == '__main__':
 
-#initialize the webcam, cap the object provided by the videoCapture
-#it contains a boolean indicating if it was successful (ret)
-# It also contains the images collected from the webcam (frame)
-cap = cv2.VideoCapture(0)
+	#initialize the webcam, cap the object provided by the videoCapture
+	#it contains a boolean indicating if it was successful (ret)
+	# It also contains the images collected from the webcam (frame)
+	cap = cv2.VideoCapture(0)
 
-while True:
-	ret, frame = cap.read()
-	cv2.imshow('Live sketcher image', sketch(frame))
-	if cv2.waitKey(1) == 13 : # 13 is enter key
-		cv2.imwrite("my_sketch.jpg",sketch(frame))
-		break
+	while True:
+		ret, frame = cap.read()
+		cv2.imshow('Live sketcher image', sketch(frame))
+		if cv2.waitKey(1) == 13 : # 13 is enter key
+			cv2.imwrite("my_sketch.jpg",sketch(frame))
+			break
 
-#release the camera and close the window
-cap.release()
-cv2.destroyAllWindows()
+	#release the camera and close the window
+	cap.release()
+	cv2.destroyAllWindows()
